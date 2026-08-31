933a3e22334145e9154fe499ed9c9ff12c3c2598
Menna Saber <mennasaper@gmail.com>
2026-08-31 22:58:44 +0300

    Question6

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            #region Question6
+            ////Declare a double price = 49.99; then convert it into an int using a cast.
+
+
+            //double price = 49.99;
+            //int intPrice = (int)price;
+            //Console.WriteLine(intPrice);
+
+            #endregion
         }
     }
 }
